171248d0e1a62261f61cf8f9ce5e9ec247449d59
Jeremy Peterson <jeremy.one@gmail.com>
2017-01-09 23:08:02 -0800

    Updates to project and tests

--- Readme.md
+++ Readme.md
@@ -1,4 +1,33 @@
-﻿# Markdown file
+﻿# CryptLink
+A peer-to-peer messaging and data storage network that uses client and server side encryption.
+
+### First Goals
+* Provide a secure and reliable network of storage and messaging to anyone participating
+* All messages and storage to may be end-to-end encrypted or signed by users
+* All server-to-server communications to be encrypted
+* Allow anyone that would like to start their own network the tools to do so
+* Do not rely on any public resources other than IP routing so it can be run on a censored Internet, or a closed LAN
+* Allow server nodes to be run on any platform supported by Mono (Linux, OS X, BSD, and Windows, including x86, x86-64, ARM, s390, PowerPC)
+
+## Future Goals
+* Create a reputation system for users
+* Create a standard API for 3rd party apps and command line users to:
+  * Get and store data
+  * Send and receive messages
+  * Send broadcast (multiple receiver) messages
+  * Push-style notifications of new messages
+  * Get network user info as provided
+* Allow browsers to participate in network using local storage
+
+## Technology
+CryptLink will use many existing technologies and ideas in new ways, including but not limited to:
+
+* TLS Trust Networks with a private root Certificate Authority for servers
+* HTTP(s) requests and sockets for general communication
+* DNSSEC for public server name resolution and bootstrapping
+* GPG/PGP for end-to-end message/data encryption and signing
+* DHT (Distributed Hash Table) for resolving the host for message, data and user data
+* Block chain for user reputation
 
 ## Licenses
-See [Lisences.md](Lisences.md)
+See [Licenses.md](Licenses.md)
--- Tests/HashTests.cs
+++ Tests/HashTests.cs
@@ -5,77 +5,130 @@ using CryptLink;
 namespace CryptLinkService {
 	[TestFixture()]
 	public class HashTests {
-		[Test()]
-		public void TestCase() {
 
-			foreach(HashProviders provider in Enum.GetValues(typeof(HashProviders))) {
-				var h1 = new Hash("TEST", provider);
-				var h2 = new Hash("TEST", provider);
-				var h3 = new Hash("test", provider);
-				var h4 = new Hash("", provider);
+        [Test()]
+        public void HashingTests() {
 
-				Assert.AreEqual(h1.Provider, provider, 
-					"HashProvider is set correctly");
+            foreach (Hash.HashProvider provider in Enum.GetValues(typeof(Hash.HashProvider))) {
+                var h1 = new Hash("TEST", provider);
+                var h2 = new Hash("TEST", provider);
+                var h3 = new Hash("test", provider);
+                var h4 = new Hash("", provider);
 
-				Assert.AreEqual(h1.Hashed, h2.Hashed, 
-					"'TEST' and 'TEST' hashes are equal for provider: '" + provider.ToString() + "'");
-				Assert.AreNotEqual(h2.Hashed, h3.Hashed, 
-					"'TEST' and 'test' hashes are NOT equal for provider: '" + provider.ToString() + "'");
+                Assert.AreEqual(h1.Provider, provider,
+                    "HashProvider is set correctly");
 
-				Assert.AreEqual(h1.CompareTo(h2), true, 
-					"Separate hashes of the same string compare returns true for provider: '" + provider.ToString() + "'");
-				Assert.AreEqual(h1.CompareTo(h3), true,
-                    "Separate hashes of the similar strings returns false for provider: '" + provider.ToString() + "'");
+                Assert.AreEqual(h1.Hashed, h2.Hashed,
+                    "'TEST' and 'TEST' hashes are equal for provider: '" + provider.ToString() + "'");
+                Assert.AreNotEqual(h2.Hashed, h3.Hashed,
+                    "'TEST' and 'test' hashes are NOT equal for provider: '" + provider.ToString() + "'");
 
-				Assert.AreEqual(h1.Rehash(99).Hashed, h2.Rehash(99).Hashed, 
-					"Rehash of the same strings returns true for provider: '" + provider.ToString() + "'");
+                Assert.True(h1.CompareTo(h2) == 0,
+                    "Separate hashes of the same string compare returns true for provider: '" + provider.ToString() + "'");
+                Assert.True(h1.CompareTo(h3) != 0,
+                    "Separate hashes of the different case strings returns false for provider: '" + provider.ToString() + "'");
 
-				Assert.AreEqual(h4.Valid, false, 
-					"Hash of empty string is not valid for provider: '" + provider.ToString() + "'");
+                Assert.AreEqual(h1.Rehash(99).Hashed, h2.Rehash(99).Hashed,
+                    "Rehash of the same strings returns true for provider: '" + provider.ToString() + "'");
 
-				//All operators hash to hash
-				Assert.AreEqual(h1 == h2, true, 
-					"Operator '==' compares correctly for provider: '" + provider.ToString() + "'");
-				Assert.AreEqual(h1 == h2, true, 
-					"Operator '!=' compares correctly for provider: '" + provider.ToString() + "'");
-				Assert.AreEqual(h1 == h2, false, 
-					"Operator '!=' compares correctly for provider: '" + provider.ToString() + "'");
-				Assert.AreEqual(h1 > h3, true, 
-					"Operator '>' compares correctly for provider: '" + provider.ToString() + "'");
-				Assert.AreEqual(h1 >= h2, true, 
-					"Operator '>=' compares correctly for provider: '" + provider.ToString() + "'");
-				Assert.AreEqual(h1 < h2, true, 
-					"Operator '<' compares correctly for provider: '" + provider.ToString() + "'");
-				Assert.AreEqual(h1 <= h2, true, 
-					"Operator '<=' compares correctly for provider: '" + provider.ToString() + "'");
-				
+                Assert.False(h4.Valid,
+                    "Hash of empty string is not valid for provider: '" + provider.ToString() + "'");
 
-				//All operators hash to binary
-				Assert.AreEqual(h1 == h2.Hashed, true, 
-					"Operator '==' compares correctly for provider (hash to binary): '" + provider.ToString() + "'");
-				Assert.AreEqual(h1 == h2.Hashed, true, 
-					"Operator '!=' compares correctly for provider (hash to binary): '" + provider.ToString() + "'");
-				Assert.AreEqual(h1 == h2.Hashed, false, 
-					"Operator '!=' compares correctly for provider (hash to binary): '" + provider.ToString() + "'");
-				Assert.AreEqual(h1 > h3.Hashed, true, 
-					"Operator '>' compares correctly for provider (hash to binary): '" + provider.ToString() + "'");
-				Assert.AreEqual(h1 >= h2.Hashed, true, 
-					"Operator '>=' compares correctly for provider (hash to binary): '" + provider.ToString() + "'");
-				Assert.AreEqual(h1 < h2.Hashed, true, 
-					"Operator '<' compares correctly for provider (hash to binary): '" + provider.ToString() + "'");
-				Assert.AreEqual(h1 <= h2.Hashed, true, 
-					"Operator '<=' compares correctly for provider (hash to binary): '" + provider.ToString() + "'");
-				
+            }
+        }
+
+        [Test()]
+        public void HashObjectOperatorTests() {
+            foreach(Hash.HashProvider provider in Enum.GetValues(typeof(Hash.HashProvider))) {
+
+                var h1 = new Hash("TEST", provider);
+                var h2 = new Hash("TEST", provider);
+                var h3 = new Hash("test", provider);
+
+                //All operators (hash to hash)
+                Assert.AreEqual(h1 == h2, true,
+                    "Operator '==' compares correctly for provider: '" + provider.ToString() + "'");
+                Assert.AreEqual(h1 == h3, false,
+                    "Operator '==' compares correctly for provider: '" + provider.ToString() + "'");
+                Assert.AreEqual(h1 != h2, false,
+                    "Operator '!=' compares correctly for provider: '" + provider.ToString() + "'");
+                Assert.AreEqual(h1 != h3, true,
+                    "Operator '!=' compares correctly for provider: '" + provider.ToString() + "'");
+
+                Assert.AreEqual(h1 > h2, false,
+                    "Operator '>' compares correctly for provider: '" + provider.ToString() + "'");
+                Assert.AreEqual(h1 > h3, true,
+                    "Operator '>' compares correctly for provider: '" + provider.ToString() + "'");
+                Assert.AreEqual(h1 < h2, false,
+                    "Operator '<' compares correctly for provider: '" + provider.ToString() + "'");
+                Assert.AreEqual(h1 < h3, false,
+                    "Operator '<' compares correctly for provider: '" + provider.ToString() + "'");
+
+                Assert.AreEqual(h1 >= h2, true,
+                    "Operator '>=' compares correctly for provider: '" + provider.ToString() + "'");
+                Assert.AreEqual(h1 >= h3, true,
+                    "Operator '>=' compares correctly for provider: '" + provider.ToString() + "'");
+                Assert.AreEqual(h1 <= h2, true,
+                    "Operator '<=' compares correctly for provider: '" + provider.ToString() + "'");
+                Assert.AreEqual(h1 <= h3, false,
+                    "Operator '<=' compares correctly for provider: '" + provider.ToString() + "'");
+
+            }
+        }
 
 
-				//All constructors, invalid objects
-				//All compare types
-				//Sorting
+        [Test()]
+        public void HashBinaryOperatorTests() {
+            foreach (Hash.HashProvider provider in Enum.GetValues(typeof(Hash.HashProvider))) {
+                var h1 = new Hash("TEST", provider);
+                var h2 = new Hash("TEST", provider);
+                var h3 = new Hash("test", provider);
 
-			}
+                //All operators (hash to binary)
+                Assert.AreEqual(h1 == h2.Hashed, true,
+                    "Operator '==' compares correctly for provider (hash to binary): '" + provider.ToString() + "'");
+                Assert.AreEqual(h1 == h3.Hashed, false,
+                    "Operator '==' compares correctly for provider (hash to binary): '" + provider.ToString() + "'");
+                Assert.AreEqual(h1 != h2.Hashed, false,
+                    "Operator '!=' compares correctly for provider (hash to binary): '" + provider.ToString() + "'");
+                Assert.AreEqual(h1 != h3.Hashed, true,
+                    "Operator '!=' compares correctly for provider (hash to binary): '" + provider.ToString() + "'");
 
+                Assert.AreEqual(h1 > h2.Hashed, false,
+                    "Operator '>' compares correctly for provider (hash to binary): '" + provider.ToString() + "'");
+                Assert.AreEqual(h1 > h3.Hashed, true,
+                    "Operator '>' compares correctly for provider (hash to binary): '" + provider.ToString() + "'");
+                Assert.AreEqual(h1 < h2.Hashed, false,
+                    "Operator '<' compares correctly for provider (hash to binary): '" + provider.ToString() + "'");
+                Assert.AreEqual(h1 < h3.Hashed, false,
+                    "Operator '<' compares correctly for provider (hash to binary): '" + provider.ToString() + "'");
 
-		}
-	}
+                Assert.AreEqual(h1 >= h2.Hashed, true,
+                    "Operator '>=' compares correctly for provider (hash to binary): '" + provider.ToString() + "'");
+                Assert.AreEqual(h1 >= h3.Hashed, true,
+                    "Operator '>=' compares correctly for provider (hash to binary): '" + provider.ToString() + "'");
+                Assert.AreEqual(h1 <= h2.Hashed, true,
+                    "Operator '<=' compares correctly for provider (hash to binary): '" + provider.ToString() + "'");
+                Assert.AreEqual(h1 <= h3.Hashed, false,
+                    "Operator '<=' compares correctly for provider (hash to binary): '" + provider.ToString() + "'");
+            }
+        }
+
+        //[Test()]
+        //public void HashOperatorTests() {
+        //    foreach (Hash.HashProvider provider in Enum.GetValues(typeof(Hash.HashProvider))) {
+        //        var h1 = new Hash("TEST", provider);
+        //        var h2 = new Hash("TEST", provider);
+        //        var h3 = new Hash("test", provider);
+        //        var h4 = new Hash("", provider);
+
+        //    }
+        //}
+
+        //Todo: Test -
+        //All constructors, invalid objects
+        //All compare types
+        //Sorting
+    }
 }
 

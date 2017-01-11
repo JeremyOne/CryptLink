445a1ac6ea8c2447a2b24f1fa35e8354f5f3cc86
Jeremy Peterson <jeremy.one@gmail.com>
2017-01-11 11:27:51 -0800

    New class for testing

---
+++ Classes/HashableString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptLink {
+
+    /// <summary>
+    /// A simple hashable object used mainly for testing
+    /// </summary>
+    class HashableString : Hashable {
+
+        public HashableString(string _Value) {
+            Value = _Value;
+        }
+
+        string Value { get; set; }
+
+        public override byte[] HashableData() {
+            if (string.IsNullOrEmpty(Value) == false) {
+                return Encoding.ASCII.GetBytes(Value);
+            } else {
+                throw new Exception("Value was null");
+            }           
+        }
+    }
+}

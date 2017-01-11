e76c24d12cd4f543daa55431028cfe006a4ef21b
Jeremy Peterson <jeremy.one@gmail.com>
2017-01-11 11:29:34 -0800

    Basic tests for ConsistantHash

--- Tests/ConsistantHashTests.cs
+++ Tests/ConsistantHashTests.cs
@@ -11,10 +11,40 @@ namespace CryptLink.Tests {
 
         [Test]
         public void ConsistantHashTests1() {
-            var shString = new ConsistentHash<string>(Hash.HashProvider.SHA128);
+            var testSize = 10000;
+            var colissions = new Dictionary<Hash.HashProvider, int>();
+            colissions.Add(Hash.HashProvider.SHA64, 9535);
+            colissions.Add(Hash.HashProvider.SHA128, 9998);
+            colissions.Add(Hash.HashProvider.SHA256, 10000);
+            colissions.Add(Hash.HashProvider.SHA512, 10000);
 
+            foreach (Hash.HashProvider provider in Enum.GetValues(typeof(Hash.HashProvider))) {
 
+                DateTime startTime = DateTime.Now;
+                var chStrings1 = new ConsistentHash<HashableString>(provider);
+                Hash lastHash = new Hash();
+                
+                for (int i = 0; i < testSize; i++) {
+                    lastHash = chStrings1.Add(new HashableString(i.ToString()), false, 1);
+                }
+
+                //smaller hash sizes may have collisions and effect the total number of items in the array
+                var colisionSize = colissions[provider];
+
+                Assert.AreEqual(chStrings1.AllNodes.Count, colisionSize, "Added all nodes in: " + (startTime - DateTime.Now).TotalMilliseconds);
+
+                DateTime startTime2 = DateTime.Now;
+                chStrings1.UpdateKeyArray();
+
+                Assert.AreEqual(chStrings1.AllNodes.Count, colisionSize, "Updated key in: " + (startTime2 - DateTime.Now).TotalMilliseconds);
+                Assert.True(chStrings1.ContainsNode(lastHash));
+                
+            }
 
         }
+
     }
+
 }
+
+

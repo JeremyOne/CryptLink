d8139f5e80d2e527de33753606e7b75ab4b150d0
Jeremy Peterson <jeremy.one@gmail.com>
2017-01-12 12:40:25 -0800

    Adding RNG helper functions

---
+++ Classes/Crypto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptLink.Classes {
+    class Crypto {
+        static RNGCryptoServiceProvider RNG = new RNGCryptoServiceProvider();
+
+        public static Guid GenerateCryptoGuid() {
+            byte[] bytes = GetBytes(16);
+            var g = new Guid(bytes);
+            return g;
+        }
+
+        public static string GetRandomString(int StringLength) {
+            return GetBytesB64(StringLength * 6);
+        }
+
+        public static byte[] GetBytes(int Length) {
+            var buffer = new byte[Length];
+            RNG.GetBytes(buffer);
+            return buffer;
+        }
+
+        public static string GetBytesB64(int ByteLength) {
+            var b = GetBytes(ByteLength);
+            return Base64.EncodeBytes(b);
+        }
+
+    }
+}

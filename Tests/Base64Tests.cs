95c257a2f2ef963ad28be8760cf9fc62871fe4c7
Jeremy Peterson <jeremy.one@gmail.com>
2017-01-13 15:41:35 -0800

    Adding tests for Base64 class

---
+++ Tests/Base64Tests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace CryptLink {
+	[TestFixture()]
+	public class Base64Tests {
+
+		[Test()]
+		public void EncodeDecodeNumberTest() {
+			var testBytes = BitConverter.GetBytes(123);
+			var encodedBytes = Base64.EncodeBytes(testBytes);
+			Assert.AreEqual(encodedBytes, "ewAAAA..");
+			var decodedBytes = Base64.DecodeBytes(encodedBytes);
+			Assert.AreEqual(testBytes, decodedBytes);
+
+			testBytes = BitConverter.GetBytes(123.456789);
+			encodedBytes = Base64.EncodeBytes(testBytes);
+			Assert.AreEqual(encodedBytes, "CwvuBzzdXkA.");
+			decodedBytes = Base64.DecodeBytes(encodedBytes);
+			Assert.AreEqual(testBytes, decodedBytes);
+
+
+		}
+
+		[Test()]
+		public void EncodeDecodeHashTest() {
+			foreach(Hash.HashProvider provider in Enum.GetValues(typeof(Hash.HashProvider))) {
+				var testHash = new Hash("Test String", provider);
+
+				var encodedBytes = Base64.EncodeBytes(testHash.Hashed);
+				var decodedBytes = Base64.DecodeBytes(encodedBytes);
+
+				Assert.True(testHash == decodedBytes);
+			}
+		}
+	}
+}
+

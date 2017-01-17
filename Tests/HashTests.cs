c08253e5a285b329b0706db12b33da18196a5427
Jeremy Peterson <jeremy.one@gmail.com>
2017-01-16 18:14:00 -0800

    Updates to hash tests afer field rename

--- Tests/HashTests.cs
+++ Tests/HashTests.cs
@@ -19,9 +19,9 @@ namespace CryptLink {
                 Assert.AreEqual(h1.Provider, provider,
                     "HashProvider is set correctly");
 
-                Assert.AreEqual(h1.Hashed, h2.Hashed,
+                Assert.AreEqual(h1.HashBytes, h2.HashBytes,
                     "'TEST' and 'TEST' hashes are equal for provider: '" + provider.ToString() + "'");
-                Assert.AreNotEqual(h2.Hashed, h3.Hashed,
+                Assert.AreNotEqual(h2.HashBytes, h3.HashBytes,
                     "'TEST' and 'test' hashes are NOT equal for provider: '" + provider.ToString() + "'");
 
                 Assert.True(h1.CompareTo(h2) == 0,
@@ -29,7 +29,7 @@ namespace CryptLink {
                 Assert.True(h1.CompareTo(h3) != 0,
                     "Separate hashes of the different case strings returns false for provider: '" + provider.ToString() + "'");
 
-                Assert.AreEqual(h1.Rehash(99).Hashed, h2.Rehash(99).Hashed,
+                Assert.AreEqual(h1.Rehash(99).HashBytes, h2.Rehash(99).HashBytes,
                     "Rehash of the same strings returns true for provider: '" + provider.ToString() + "'");
 
                 Assert.False(h4.Valid,
@@ -85,31 +85,31 @@ namespace CryptLink {
                 var h3 = new Hash("test", provider);
 
                 //All operators (hash to binary)
-                Assert.AreEqual(h1 == h2.Hashed, true,
+                Assert.AreEqual(h1 == h2.HashBytes, true,
                     "Operator '==' compares correctly for provider (hash to binary): '" + provider.ToString() + "'");
-                Assert.AreEqual(h1 == h3.Hashed, false,
+                Assert.AreEqual(h1 == h3.HashBytes, false,
                     "Operator '==' compares correctly for provider (hash to binary): '" + provider.ToString() + "'");
-                Assert.AreEqual(h1 != h2.Hashed, false,
+                Assert.AreEqual(h1 != h2.HashBytes, false,
                     "Operator '!=' compares correctly for provider (hash to binary): '" + provider.ToString() + "'");
-                Assert.AreEqual(h1 != h3.Hashed, true,
+                Assert.AreEqual(h1 != h3.HashBytes, true,
                     "Operator '!=' compares correctly for provider (hash to binary): '" + provider.ToString() + "'");
 
-                Assert.AreEqual(h1 > h2.Hashed, false,
+                Assert.AreEqual(h1 > h2.HashBytes, false,
                     "Operator '>' compares correctly for provider (hash to binary): '" + provider.ToString() + "'");
-                Assert.AreEqual(h1 > h3.Hashed, true,
+                Assert.AreEqual(h1 > h3.HashBytes, true,
                     "Operator '>' compares correctly for provider (hash to binary): '" + provider.ToString() + "'");
-                Assert.AreEqual(h1 < h2.Hashed, false,
+                Assert.AreEqual(h1 < h2.HashBytes, false,
                     "Operator '<' compares correctly for provider (hash to binary): '" + provider.ToString() + "'");
-                Assert.AreEqual(h1 < h3.Hashed, false,
+                Assert.AreEqual(h1 < h3.HashBytes, false,
                     "Operator '<' compares correctly for provider (hash to binary): '" + provider.ToString() + "'");
 
-                Assert.AreEqual(h1 >= h2.Hashed, true,
+                Assert.AreEqual(h1 >= h2.HashBytes, true,
                     "Operator '>=' compares correctly for provider (hash to binary): '" + provider.ToString() + "'");
-                Assert.AreEqual(h1 >= h3.Hashed, true,
+                Assert.AreEqual(h1 >= h3.HashBytes, true,
                     "Operator '>=' compares correctly for provider (hash to binary): '" + provider.ToString() + "'");
-                Assert.AreEqual(h1 <= h2.Hashed, true,
+                Assert.AreEqual(h1 <= h2.HashBytes, true,
                     "Operator '<=' compares correctly for provider (hash to binary): '" + provider.ToString() + "'");
-                Assert.AreEqual(h1 <= h3.Hashed, false,
+                Assert.AreEqual(h1 <= h3.HashBytes, false,
                     "Operator '<=' compares correctly for provider (hash to binary): '" + provider.ToString() + "'");
             }
         }

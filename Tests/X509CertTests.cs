edcd5f80b606397a8d753a6ffd4f7a6ea01ba416
Jeremy Peterson <jeremy.one@gmail.com>
2017-01-20 20:59:11 -0800

    Many updates to x509 Cert Tests
    All methods now assert and pass

--- Tests/X509CertTests.cs
+++ Tests/X509CertTests.cs
@@ -2,36 +2,59 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using static CryptLink.X509Certificate2Extentions;
 
-namespace CryptLink {
+namespace CryptLink.Tests {
 
     [TestFixture]
     public class TestX509CertBuilder{
         [Test]
-        public void TestMethod() {
+        public void TestX509CertBuilderTests() {
 
             var ca1 = new X509Certificate2Builder { SubjectName = "CN=Test CA1" }.Build();
-            var in1A = new X509Certificate2Builder { SubjectName = "CN=Intermediate 1A", Issuer = ca1 }.Build();
-            var in1B = new X509Certificate2Builder { SubjectName = "CN=Intermediate 1B", Issuer = in1A }.Build();
-            var cert1 = new X509Certificate2Builder { SubjectName = "CN=Test 1", Issuer = in1B, Intermediate = false }.Build();
-            var cert1B = new X509Certificate2Builder { SubjectName = "CN=Test 1B", Issuer = cert1 }.Build();
+            Assert.True(Utility.VerifyCert(ca1, true, false, null), "CA1 is valid (AllowUnknownCertificateAuthority = true)");
+
+            var intermediate1 = new X509Certificate2Builder { SubjectName = "CN=Intermediate 1", Issuer = ca1 }.Build();
+            Assert.False(Utility.VerifyCert(intermediate1, false, false, null), "Intermediate 1 is not valid when CA not provided, and AllowUnknownCA is false");
+            Assert.True(Utility.VerifyCert(intermediate1, false, false, ca1), "Intermediate 1 is valid with: CA1");
+
+            var intermediate2 = new X509Certificate2Builder { SubjectName = "CN=Intermediate 2", Issuer = intermediate1 }.Build();
+            Assert.True(Utility.VerifyCert(intermediate2, false, false, ca1, intermediate1), "Intermediate 2 is valid with: CA1.Intermediate1");
+
+            var cert1 = new X509Certificate2Builder { SubjectName = "CN=Test 1", Issuer = intermediate1, Intermediate = false }.Build();
+            Assert.True(Utility.VerifyCert(cert1, false, false, ca1, intermediate1), "Cert 1 is valid with: CA1.intermediate1");
+            Assert.False(Utility.VerifyCert(cert1, false, false, ca1, intermediate2), "Cert 1 is NOT valid checked against intermediate 2");
+
+            var cert2 = new X509Certificate2Builder { SubjectName = "CN=Test 2", Issuer = intermediate2 }.Build();
+            Assert.True(Utility.VerifyCert(cert2, false, false, ca1, intermediate1, intermediate2), "Cert 2 is valid with: CA1.intermediate1.intermediate2");
+
+            var cert3 = new X509Certificate2Builder { SubjectName = "CN=Test 3", Issuer = cert2 }.Build();
+            Assert.True(Utility.VerifyCert(cert3, false, false, ca1, intermediate1, intermediate2, cert2), "Cert 3 is valid with: CA1.intermediate1.intermediate2.Cert2");
+
 
             var ca2 = new X509Certificate2Builder { SubjectName = "CN=Test CA2" }.Build();
-            var cert2 = new X509Certificate2Builder { SubjectName = "CN=Test 2", Issuer = ca2, Intermediate = false }.Build();
+            Assert.True(Utility.VerifyCert(ca2, true, false, null), "CA2 is valid (AllowUnknownCertificateAuthority = true)");
 
-            var invalidCert1 = new X509Certificate2Builder {
+            Assert.False(Utility.VerifyCert(intermediate1, false, false, ca2), "Intermediate 1A is NOT valid and checked against CA2");
+
+            var invalidCert3 = new X509Certificate2Builder {
                 SubjectName = "CN=Invalid 1",
                 IssuerName = ca1.SubjectName.Name,
                 IssuerPrivateKey = ca2.PrivateKey
             }.Build();
 
-            var invalidCert2 = new X509Certificate2Builder {
+            Assert.False(Utility.VerifyCert(invalidCert3, false, false, ca2), "Cert 3 is NOT valid when generated with incorrect issuer name");
+
+            var invalidCert4 = new X509Certificate2Builder {
                 SubjectName = "CN=Invalid 2",
                 Issuer = ca2,
                 NotBefore = DateTime.Now.AddDays(1)
             }.Build();
+
+            Assert.False(Utility.VerifyCert(invalidCert4, false, false, ca2), "Cert 4 is NOT valid with future date");
 
         }
     }

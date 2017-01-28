0abf23c735d4c2e56dcc11f63b6ff465678cfd70
Jeremy Peterson <jeremy.one@gmail.com>
2017-01-27 23:42:58 -0800

    Stubs for Block class

---
+++ Classes/Block.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CryptLink {
+	public class Block<T> : Hashable {
+
+		public Hash ParentHash { get; set; }
+		public Hash GeneratorAddress { get; set; }
+		public DateTime GenerationDate { get; set; }
+		public long BlockNumber { get; set; }
+		public List<T> BlockItems { get; set; }
+
+		public bool Verify(Block<T> Parent){
+			throw new NotImplementedException();
+		}
+
+        public override byte[] HashableData() {
+            throw new NotImplementedException();
+        }
+    }
+}
+

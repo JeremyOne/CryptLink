d21cb517e08d7fbc92db7c307219df7c92162ae3
Jeremy Peterson <jeremy.one@gmail.com>
2017-02-09 15:09:33 -0800

    Refactoring "Server" to "Peer" and "Peer" to "User"
    Peer = Network host
    User = Network user

---
+++ Classes/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptLink {
+
+    /// <summary>
+    /// User - A Users of the network
+    /// </summary>
+    class User : Hashable {
+
+        public byte[] PublicKey { get; set; }
+        public string Name { get; set; }
+        public DateTime LastActivity { get; set; }
+
+        public override byte[] HashableData() {
+            return PublicKey;
+        }
+
+    }
+}

6cff72e652993758ed17d41deb6f0b357c535747
Jeremy Peterson <jeremy.one@gmail.com>
2017-02-16 14:32:30 -0800

    Adding stubs for BlockItem and Server classes

---
+++ Classes/BlockItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptLink {
+    public class BlockItem<T> : Hashable
+        where T : Hashable {
+
+        public string Name { get; set; }
+        public Hash Owner { get; set; }
+        public DateTime Age { get; set; }
+        public T Value { get; set; }
+		public BlockItemStatus Status { get; set; }
+		public int ConfirmedNodes { get; set; }
+		public int UnconfirmedNodes { get; set; }
+
+        public override byte[] HashableData() {
+            return
+	            Encoding.ASCII.GetBytes(Name)
+	             .Concat(Owner.HashBytes)
+	             .Concat(BitConverter.GetBytes(Age.Ticks))
+	             .Concat(Value.HashableData()).ToArray();
+        }
+    }
+
+	public enum BlockItemStatus{
+		Pending,
+		Comitted,
+		Confirmed,
+		Expired,
+		Unknowm
+	}
+
+}
---
+++ Classes/Server.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptLink {
+
+    /// <summary>
+    /// A server that runs locally, processes requests, caches data
+    /// </summary>
+    public class Server {
+
+    }
+
+}

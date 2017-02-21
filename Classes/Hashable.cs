e7cb37cd367741dda11ef10a392124b033ed5213
Jeremy Peterson <jeremy.one@gmail.com>
2017-02-21 01:51:39 -0800

    New functions useful to Hashable

--- Classes/Hashable.cs
+++ Classes/Hashable.cs
@@ -22,6 +22,21 @@ namespace CryptLink
         public Hash GetHash(Hash.HashProvider Provider) {
             return new Hash(HashableData(), Provider);
         }
+        
+        /// <summary>
+        /// Gets a Hash object from a object that inherits from Hashable
+        /// </summary>
+        /// <param name="FromObject">Object to hash</param>
+        /// <param name="Provider">The hash provider to hash with</param>
+        public static Hash GetHash<H>(H FromObject, Hash.HashProvider Provider) where H : Hashable {
+            return FromObject.GetHash(Provider);
+        }
 
+        /// <summary>
+        /// Returns if the specified object is a subclass of Hashable
+        /// </summary>
+        public static bool IsHashable(object obj) {
+            return obj.GetType().IsSubclassOf(typeof(Hashable));
+        }
     }
 }

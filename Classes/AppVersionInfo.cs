14bf7b93968ee42576f2f772e22cc86683dd62b9
Jeremy Peterson <jeremy.one@gmail.com>
2017-04-03 19:45:56 -0700

    Adding AppVersionInfo
    Useful for reporting application and API versions to clients

---
+++ Classes/AppVersionInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CryptLink {
+
+	public class AppVersionInfo : Hashable {
+
+		/// <summary>
+		/// The name of this application
+		/// </summary>
+		public string Name { get; private set;}  
+
+		/// <summary>
+		/// The version of this application
+		/// </summary>
+		/// <value>The version.</value>
+		public Version Version { get; private set; }
+
+		/// <summary>
+		/// The latest version of the CryptLink API implemented
+		/// </summary>
+		public Version ApiVersion { get; private set; }
+
+		/// <summary>
+		/// The oldest version of the CryptLink API that is supported by this application
+		/// </summary>
+		public Version ApiCompartibilityVersion { get; private set; }
+	}
+}
+

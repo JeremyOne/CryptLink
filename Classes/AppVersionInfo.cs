eb6d6ff48aeef9f8e166b36e2995b152e5f74388
Jeremy Peterson <jeremy.one@gmail.com>
2017-04-26 22:09:09 -0700

    Update to version

--- Classes/AppVersionInfo.cs
+++ Classes/AppVersionInfo.cs
@@ -1,29 +1,32 @@
 ﻿using System;
+using System.Text;
+using System.Linq;
 
 namespace CryptLink {
 
-	public class AppVersionInfo : Hashable {
+	public class AppVersionInfo {
 
 		/// <summary>
 		/// The name of this application
 		/// </summary>
-		public string Name { get; private set;}  
+		public string Name { get; set; }  
 
 		/// <summary>
 		/// The version of this application
 		/// </summary>
 		/// <value>The version.</value>
-		public Version Version { get; private set; }
+		public Version Version { get; set; }
 
 		/// <summary>
 		/// The latest version of the CryptLink API implemented
 		/// </summary>
-		public Version ApiVersion { get; private set; }
+		public Version ApiVersion { get; set; }
 
 		/// <summary>
 		/// The oldest version of the CryptLink API that is supported by this application
 		/// </summary>
-		public Version ApiCompartibilityVersion { get; private set; }
-	}
+		public Version ApiCompartibilityVersion { get; set; }
+
+    }
 }
 

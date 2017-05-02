e929ffc78dc958236825ff51cccb97aabf643b50
Jeremy Peterson <jeremy.one@gmail.com>
2017-05-01 18:32:34 -0700

    Several updates to peer

--- Classes/Peer.cs
+++ Classes/Peer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ServiceStack;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,6 +8,7 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace CryptLink {
 
@@ -18,7 +20,7 @@ namespace CryptLink {
         public DateTime LastSeen { get; set; }
         public DateTime FirstSeen { get; set; }
         public Uri LastKnownPublicUri { get; set; }
-        public List<Uri> KnownPublicIPs { get; set; }
+        public List<Uri> KnownPublicUris { get; set; }
         public List<Hash> KnownPeers { get; set; }
         public Hash ServerOperator { get; set; }
         public X509Certificate2 PublicKey { get; set; }
@@ -26,13 +28,12 @@ namespace CryptLink {
         public int ConnectRetryMax { get; set; }
         public int ConnectRetries { get; private set; }
         public AppVersionInfo Version { get; set; }
+		public override Hash.HashProvider Provider { get; set; }
+        public int Weight { get; set; } = 50;
 
-        //private static readonly ILog = LogManger.GetLogger(typeof(this));
+        public IServiceClient LocalClient { get; set; }
 
-        public override Hash.HashProvider Provider { get; set; }
-
-        public ClientWebSocket ClientSocket { get; private set; }
-
+		[JsonIgnore]
         public override bool HashIsImmutable {
             get {
                 return true;
@@ -40,65 +41,23 @@ namespace CryptLink {
         }
 
         public override byte[] HashableData() {
-            return PublicKey.PublicKey.EncodedKeyValue.RawData;
+            if (PublicKey == null) {
+                throw new NullReferenceException("Public key of a peer can not be null");
+            } else {
+                return PublicKey.PublicKey.EncodedKeyValue.RawData;
+            }
         }
 
         public bool TryConnect() {
-            if (Connected()) {
-                return true;
-            } else {
-                throw new NotImplementedException();
-            }
-
-            ConnectRetries = 0;
-
-            if (ClientSocket == null) {
-                //no client (yet)
-                //var wscli = new ClientWebSocket();
-                //var tokSrc = new CancellationTokenSource();
-                //var task = wscli.ConnectAsync(LastKnownPublicUri, tokSrc.Token);
-
-                //task.Wait();
-                //task.Dispose();
-
-                //Console.WriteLine($"WebSocket to {this.ToString()} {LastKnownPublicUri} OPEN!");
-                //Console.WriteLine("SubProtocol: " + wscli.SubProtocol ?? "");
-
-                //Console.WriteLine(@"Type ""exit<Enter>"" to quit... ");
-                //for (var inp = Console.ReadLine(); inp != "exit"; inp = Console.ReadLine()) {
-                //    task = wscli.SendAsync(
-                //        new ArraySegment<byte>(Encoding.UTF8.GetBytes(inp)),
-                //        WebSocketMessageType.Text,
-                //        true,
-                //        tokSrc.Token
-                //    );
-
-                //    task.Wait();
-                //    task.Dispose();
-                //    Console.WriteLine("**** sent msg");
-                //}
-
-                //if (wscli.State == WebSocketState.Open) {
-                //    task = wscli.CloseAsync(WebSocketCloseStatus.NormalClosure, "", tokSrc.Token);
-                //    task.Wait();
-                //    task.Dispose();
-                //}
-
-                //tokSrc.Dispose();
-                //Console.WriteLine("WebSocket CLOSED");
-
-                
-            }
+            throw new NotImplementedException();
         }
 
         public bool Connected() {
-            return (ClientSocket?.State == WebSocketState.Open);
+            throw new NotImplementedException();
         }
 
         public void Dispose() {
-            if (Connected()) {
-                ClientSocket.Dispose();
-            }
+            throw new NotImplementedException();
         }
 
     }
